943e6dbd3f048720867bb8a1181be7c45ee42a7b
Zac <zac.m@timetabling.com.au>
2019-04-16 14:58:29 +1000

    Moved processing into TypeDef Class

--- JSDoc_TypeDef_Generator/JSDoc/TypeDef.cs
+++ JSDoc_TypeDef_Generator/JSDoc/TypeDef.cs
@@ -3,23 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace JSDoc_TypeDef_Generator.JSDoc
-{
-    class TypeDef
-    {
+namespace JSDoc_TypeDef_Generator.JSDoc { 
+    class TypeDef {
         public string Name { get; set; }
         public string Description { get; set; }
         public PropertyList Properties { get; set; }
 
-        public TypeDef(string Name, string Description)
-        {
+        public TypeDef(string Name, string Description) {
             this.Name = Name;
             this.Description = Description;
             Properties = new PropertyList();
         }
 
-        public override string ToString()
-        {
+        public override string ToString() {
             string result = $"{Description}\n@typedef {{Object}} {Name}";
             string props = Properties.ToString();
             if (props != "") result += "\n" + props;
@@ -29,6 +25,13 @@ namespace JSDoc_TypeDef_Generator.JSDoc
         public static TypeDef[] Parse(string str) {
             List<TypeDef> result = new List<TypeDef>();
             object JSON = JsonConvert.DeserializeObject(str);
+
+            TypeDef parseObj(object obj) {
+            }
+            TypeDef parseArr(object[] arr) {
+
+            }
         }
+        
     }
 }
