b19779f99b5c60026d8f080ec763a95144026568
Zac <zac.m@timetabling.com.au>
2019-04-16 11:56:53 +1000

    Added initial argument handling

--- JSDoc_TypeDef_Generator/Program.cs
+++ JSDoc_TypeDef_Generator/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Mono.Options;
+using System;
+using System.Collections.Generic;
 
 namespace JSDoc_TypeDef_Generator
 {
@@ -6,7 +8,23 @@ namespace JSDoc_TypeDef_Generator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string JSONString;
+            if (args.Length == 1)
+            {
+                JSONString = args[0];
+            }
+            else {
+#if DEBUG
+                Console.WriteLine("Enter JSON Object:");
+                JSONString = Console.ReadLine();
+#else
+                Console.Error.WriteLine("This utility generates a JSDOC TypeDef comment based on a JSON object");
+                Console.Error.WriteLine("Usage: gentypedef <JSON Object>");
+                return;
+#endif
+            }
+
+            Console.ReadLine();
         }
     }
 }
