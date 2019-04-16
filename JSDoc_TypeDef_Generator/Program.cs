171255ff7fa9948fc24336d7beffdc81148c2a53
Zac <zac.m@timetabling.com.au>
2019-04-16 11:09:29 +1000

    Add project files.

---
+++ JSDoc_TypeDef_Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSDoc_TypeDef_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
