d7a1ee1f4a41041347e7c377a4c2dae3fc8298f9
Zac <zac.m@timetabling.com.au>
2019-04-17 10:02:44 +1000

    Moved processing of actual jsdoc type to the type class

--- JSDoc_TypeDef_Generator/JSDoc/Type.cs
+++ JSDoc_TypeDef_Generator/JSDoc/Type.cs
@@ -1,23 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace JSDoc_TypeDef_Generator.JSDoc
 {
-    class Type
+    class JSDType
     {
-        public static string Boolean { get => "boolean"; }
-        public static string Number { get => "number"; }
-        public static string String { get => "string"; }
+        public string[] Types { get; }
+        public NullableStatus Nullable { get; set; }
+        public bool IsArray { get; set; }
 
-        public static string TranslateType(Type t) {
+        public override string ToString() {
+            string tt = string.Join("|", Types);
+            if (Types.Length > 1) tt = $"({tt})";
+            if (Nullable == NullableStatus.NonNullable) tt = "!" + tt;
+            else if (Nullable == NullableStatus.Nullable) tt = "?" + tt;
+            return $"{{{tt}}}";
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is JSDType)) return false;
+            JSDType tobj = (JSDType)obj;
+            return Types.OrderBy(x => x).SequenceEqual(tobj.Types.OrderBy(x => x)) &&
+                   Nullable == tobj.Nullable &&
+                   IsArray == tobj.IsArray;
+        }
+
+        public override int GetHashCode() {
+            string s = string.Join("", Types.OrderBy(x => x));
+            s += (int)Nullable;
+            s += IsArray?"A":"X";
+            return s.GetHashCode();
+        }
+
+        public JSDType(params string[] Types) {
+            Nullable = NullableStatus.Unspecified;
+            this.Types = Types;
+        }
+
+        public static JSDType Boolean { get => new JSDType("boolean"); }
+        public static JSDType Number { get => new JSDType("number"); }
+        public static JSDType String { get => new JSDType("string"); }
+        public static JSDType Any { get => new JSDType(); }
+
+        public static JSDType TranslatePrimitive(Type t) {
             if (t.Equals(typeof(string))) return String;
             if (t.Equals(typeof(int))) return Number;
             if (t.Equals(typeof(double))) return Number;
             if (t.Equals(typeof(float))) return Number;
             if (t.Equals(typeof(decimal))) return Number;
             if (t.Equals(typeof(bool))) return Boolean;
-            return null;
+            return Any;
+        }
+
+        public enum NullableStatus {
+            Unspecified,
+            Nullable,
+            NonNullable
         }
     }
 }
