a43bb44ecd11d5478087f755d2de8a57ffa958a3
Zac <zac.m@timetabling.com.au>
2019-04-17 10:23:25 +1000

    Finished up recursive algorithm in typedef.cs

--- JSDoc_TypeDef_Generator/JSDoc/TypeDef.cs
+++ JSDoc_TypeDef_Generator/JSDoc/TypeDef.cs
@@ -31,65 +31,51 @@ namespace JSDoc_TypeDef_Generator.JSDoc {
             List<TypeDef> result = new List<TypeDef>();
             JSDType parseObj(object obj) {
                 Type t = obj.GetType();
-                if (t.IsArray) { return parseArr((object[])obj); }
+                if (t.IsArray) return parseArr((object[])obj);
+                if (!t.IsClass) return JSDType.TranslatePrimitive(t);
+
                 TypeDef current = new TypeDef(defaultDescription + result.Count);
                 var props = t.GetProperties();
                 foreach (var prop in props) {
-                    JSDType jsdt;
-                    if (prop.PropertyType.IsClass)
-                        jsdt = parseObj(prop.GetValue(obj));
-                    else if (prop.PropertyType.IsArray)
-                        jsdt = parseArr((object[])prop.GetValue(obj));
-                    else
-                        jsdt = JSDType.TranslatePrimitive(prop.PropertyType);
-
+                    JSDType jsdt = parseObj(prop.GetValue(obj));
                     current.Properties.Add(prop.Name, jsdt);
                 }
-
-                result.Add(current);
-                return current.JSDType;
+                
+                return squashTypes(current).JSDType;
             }
             JSDType parseArr(object[] arr) {
                 if (arr.Length == 0) return new JSDType() { IsArray = true };
                 Type[] ts = arr.Select(x => x.GetType()).ToArray();
                 int classCount = ts.Count(x => x.IsClass);
 
-                List<string> arrTypes = new List<string>();
+                List<JSDType> arrTypes = new List<JSDType>();
                 foreach (var elem in arr) {
-                    Type ctype = elem.GetType();
-                    if ()
-                    JSDType tName = JSDType.TranslatePrimitive(ctype);
-                    if (tName != null) {
-                        if (!arrTypes.Contains(tName))
-                            arrTypes.Add(tName);
-                    } else {
-                        TypeDef cc = parseObj(elem);
-                    }
+                    arrTypes.Add(parseObj(elem));
                 }
-
-                if (classCount == 0) {
-                    //primitive array
-                    var tsd = ts.Distinct();
-                    string tName = string.Join("|", tsd.Select(x => JSDType.TranslatePrimitive(x)));
-                    if (tsd.Count() > 1) tName = $"({tName})";
-                    return new TypeDef(tName);
-                } else if (classCount == arr.Length) {
-                    //object array
-
-                } else {
-                    //mixed primitive/object array
-                    return new TypeDef("");
-                }
-                TypeDef current;
-
+                return new JSDType(arrTypes.SelectMany(x => x.Types).Distinct().ToArray());
             }
             TypeDef squashTypes(TypeDef t) {
                 foreach (TypeDef td in result) {
+                    if (t.Properties.All(x => td.Properties.Contains(x))) {
+                        // equivalent type already exists
+                        return td;
+                    } else if (td.Properties.All(x => t.Properties.Contains(x))) {
+                        // squash this type into existing type
+                        td.Properties = t.Properties;
+                        return td;
+                    }
                 }
-                this.All(x => other.Any(y => y.Equivalent(x)));
+                result.Add(t);
+                return t;
             } 
             object JSON = JsonConvert.DeserializeObject(str);
-            parseObj(JSON);
+            JSDType rootType = parseObj(JSON);
+
+            TypeDef rootTypeDef = result.First(x => x.JSDType == rootType);
+            result.Remove(rootTypeDef);
+            result.Add(rootTypeDef);
+            result.Reverse();// move root typedef to the front of list
+
             return result.ToArray();
         }
     }
