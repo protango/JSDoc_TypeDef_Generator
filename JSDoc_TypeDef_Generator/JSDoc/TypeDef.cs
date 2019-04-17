9a5cce6b76a238fed74ef6642d89399e7fbc8618
Zac <zac.m@timetabling.com.au>
2019-04-17 10:03:26 +1000

    started to write the squashTypes method

--- JSDoc_TypeDef_Generator/JSDoc/TypeDef.cs
+++ JSDoc_TypeDef_Generator/JSDoc/TypeDef.cs
@@ -12,6 +12,7 @@ namespace JSDoc_TypeDef_Generator.JSDoc {
         public string Name { get; set; }
         public string Description { get; set; }
         public PropertyList Properties { get; set; }
+        public JSDType JSDType { get => new JSDType(Name); }
 
         public TypeDef(string Name = defaultName, string Description = defaultDescription) {
             this.Name = Name;
@@ -28,26 +29,68 @@ namespace JSDoc_TypeDef_Generator.JSDoc {
 
         public static TypeDef[] Parse(string str) {
             List<TypeDef> result = new List<TypeDef>();
-            TypeDef parseObj(object obj) {
+            JSDType parseObj(object obj) {
                 Type t = obj.GetType();
                 if (t.IsArray) { return parseArr((object[])obj); }
                 TypeDef current = new TypeDef(defaultDescription + result.Count);
                 var props = t.GetProperties();
                 foreach (var prop in props) {
+                    JSDType jsdt;
                     if (prop.PropertyType.IsClass)
-                        parseObj(prop.GetValue(obj));
+                        jsdt = parseObj(prop.GetValue(obj));
+                    else if (prop.PropertyType.IsArray)
+                        jsdt = parseArr((object[])prop.GetValue(obj));
                     else
+                        jsdt = JSDType.TranslatePrimitive(prop.PropertyType);
 
-                    current.Properties.Add(prop.Name, prop.PropertyType);
+                    current.Properties.Add(prop.Name, jsdt);
                 }
+
+                result.Add(current);
+                return current.JSDType;
             }
-            TypeDef parseArr(object[] arr) {
+            JSDType parseArr(object[] arr) {
+                if (arr.Length == 0) return new JSDType() { IsArray = true };
+                Type[] ts = arr.Select(x => x.GetType()).ToArray();
+                int classCount = ts.Count(x => x.IsClass);
+
+                List<string> arrTypes = new List<string>();
+                foreach (var elem in arr) {
+                    Type ctype = elem.GetType();
+                    if ()
+                    JSDType tName = JSDType.TranslatePrimitive(ctype);
+                    if (tName != null) {
+                        if (!arrTypes.Contains(tName))
+                            arrTypes.Add(tName);
+                    } else {
+                        TypeDef cc = parseObj(elem);
+                    }
+                }
+
+                if (classCount == 0) {
+                    //primitive array
+                    var tsd = ts.Distinct();
+                    string tName = string.Join("|", tsd.Select(x => JSDType.TranslatePrimitive(x)));
+                    if (tsd.Count() > 1) tName = $"({tName})";
+                    return new TypeDef(tName);
+                } else if (classCount == arr.Length) {
+                    //object array
+
+                } else {
+                    //mixed primitive/object array
+                    return new TypeDef("");
+                }
+                TypeDef current;
 
             }
+            TypeDef squashTypes(TypeDef t) {
+                foreach (TypeDef td in result) {
+                }
+                this.All(x => other.Any(y => y.Equivalent(x)));
+            } 
             object JSON = JsonConvert.DeserializeObject(str);
             parseObj(JSON);
             return result.ToArray();
         }
-        
     }
 }
